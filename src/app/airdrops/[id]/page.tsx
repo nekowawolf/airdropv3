5e20da4b92e8b3309fa9f9953c69fd8e401955b8
nekowawolf <irukainu20@gmail.com>
2026-06-01 21:05:24 +0700

    feat: add dynamic airdrop metadata

--- src/app/airdrops/[id]/page.tsx
+++ src/app/airdrops/[id]/page.tsx
@@ -1,11 +1,20 @@
-
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import DetailClient from './DetailClient';
 
 import { dashboardMetadata } from '@/constants/metadataTemplates';
+import { fetchAirdropById } from '@/services/airdropService';
 
-export const metadata = dashboardMetadata('Airdrops', 'Explore ongoing and completed airdrops.');
+type Props = {
+    params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+    const resolvedParams = await params;
+    const airdrop = await fetchAirdropById(resolvedParams.id);
+    if (!airdrop) return dashboardMetadata("Not Found", "Airdrop not found");
+    return dashboardMetadata(airdrop.name, airdrop.description);
+}
 
 export default function AirdropsPage() {
     return (
