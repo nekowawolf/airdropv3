1a72b00983677bfad88a1cb6ed20c88498a030e4
nekowawolf <irukainu20@gmail.com>
2026-06-19 08:57:26 +0700

    feat: migrate to CDN assets and add light mode support

--- src/components/Godhand.tsx
+++ src/components/Godhand.tsx
@@ -1,26 +1,25 @@
 'use client';
 
-import Image from "next/image";
 import Link from "next/link";
 
 export default function GodhandSection() {
     return (
         <section id="alpha" className="flex items-center justify-center relative">
-            <Image
-                src="/images/godhand.png"
-                alt="Explore Airdrop"
-                width={1600}
-                height={1600}
-                className="object-cover"
-                priority
+            <img
+                src="https://nekowawolf.github.io/cdn-images/images/2026/1781829282_godhand.png"
+                alt="Explore Airdrop Dark"
+                className="theme-icon-sun object-cover w-full h-full"
+            />
+            <img
+                src="https://nekowawolf.github.io/cdn-images/images/2026/1781831285_godhand_white.png"
+                alt="Explore Airdrop Light"
+                className="theme-icon-moon object-cover w-full h-full"
             />
 
             <Link href="/airdrops" className="absolute flex flex-col items-center sm:mr-14 mr-7">
-                <Image
-                    src="/images/folder.png"
+                <img
+                    src="https://nekowawolf.github.io/cdn-images/images/2026/1781829491_folder.png"
                     alt="Folder"
-                    width={96}
-                    height={96}
                     className="w-14 sm:w-24 h-14 sm:h-24 cursor-pointer"
                 />
                 <p className="-mt-2 text-fill-color text-[10px] sm:text-xl font-semibold drop-shadow-md">
